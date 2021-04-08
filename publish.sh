e09af0efa7766ac6904731106ad23058ae28f13e
boy12371 <boy12371@hotmail.com>
2021-04-08 23:13:07 +0800

    20210408231302

--- publish.sh
+++ publish.sh
@@ -2,4 +2,3 @@
 #
 github-release release --user boy12371 --repo refpolicy --tag v2.20151208 --pre-release
 github-release upload  --user boy12371 --repo refpolicy --tag v2.20151208 --file ./policy.31 --name policy.31
-github-release upload  --user boy12371 --repo refpolicy --tag v2.20151208 --file ./policy.29 --name policy.29
